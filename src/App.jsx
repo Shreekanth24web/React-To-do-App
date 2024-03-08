c3b415b51f3d4fe65db6cf59fdf7911f6dfb9641
shreekanth24web <shreekanth249@gmail>
2024-03-08 10:09:20 +0530

    Ignoring txt files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
-import Conditional from "./ConditionalRendering/Conditional";
-import HideShow from "./ConditionalRendering/HideShow";
-import Todo from "./Todo/Todo";
 import TodoList from "./Todo/TodoList";
-import FormValidation from "./Validation/FormValidation";
-import FormData from "./components/FormData";
-import Forms from "./components/Forms";
-import Lists from "./components/Lists";
-import Reactjsx from "./components/Reactjsx";
-import Render from "./components/Render";
 
 function App() {
   return (
